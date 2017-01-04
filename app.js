50a959da3d323bf71510ed4a8b09315dc7caa4c3
quoc bao <tranquocbaodev@gmail.com>
2017-01-05 00:07:13 +0700

    update-5-1-2017

--- app.js
+++ app.js
@@ -44,4 +44,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+var port = process.env.PORT || 3001;
+app.listen(port, function () {
+  console.log('server listening on port %d in %s mode', port, app.get('env'));
+});
+
+
 module.exports = app;
